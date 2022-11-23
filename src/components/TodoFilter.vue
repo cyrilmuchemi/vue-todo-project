ea37f7b1c8f0d6d88ac8a42a68d178ed7f65a1b1
cyrilmuchemi <cyrilmuchemi@gmail.com>
2022-11-23 23:20:20 +0300

    Enable some key functionalities

---
+++ src/components/TodoFilter.vue
@@ -0,0 +1,15 @@
+<template>
+    <p
+      @click="select"
+      :class="{ 'menu-active': props.modelValue == props.title }"
+    >
+      {{ props.title }}
+    </p>
+  </template>
+  <script setup>
+  let props = defineProps(["title", "modelValue"]);
+  let emit = defineEmits(["update:modelValue"]);
+  function select() {
+    emit("update:modelValue", props.title);
+  }
+</script>
